78a6e145486e804736187bd27a89d0d8afc7a063
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-03-03 21:23:17 +0400

    IDK

--- classes/town.h
+++ classes/town.h
@@ -3,43 +3,48 @@
 using namespace std;
 
 const int MAX_RESIDENTS = 100;
+const int MAX_TOWNS = 50;
 
-class Town 
+class Town
 {
 private:
     string name;
-    Person residents[ MAX_RESIDENTS ];
+    Person residents[MAX_RESIDENTS];
     int current_residentsNumber;
+    string traversableTowns[MAX_TOWNS];
+
 public:
-    Town(string name ) 
+    Town(string name, int residentsNumber, string traversableTowns)
     {
         this->name = name;
-        this->current_residentsNumber = 0;
+        this->current_residentsNumber = residentsNumber;
+        this->traversableTowns = traversableTowns;
     }
 
-    void changeResidentsNumber( int change ) 
+    void changeResidentsNumber(int change)
     {
         this->current_residentsNumber += change;
     }
 
-
     // Setters
-    void setName(const std::string& name) {
+    void setName(const std::string &name)
+    {
         this->name = name;
     }
 
-    void setCurrentResidentsNumber(int current_residentsNumber) {
+    void setCurrentResidentsNumber(int current_residentsNumber)
+    {
         this->current_residentsNumber = current_residentsNumber;
     }
 
-
     // Getters
-    string getName() const {
+    string getName() const
+    {
         return name;
     }
 
-    int getCurrentResidentsNumber() const {
+    int getCurrentResidentsNumber() const
+    {
         return current_residentsNumber;
     }
-
 };
--- engine.cpp
+++ engine.cpp
@@ -1,18 +1,109 @@
 #include <iostream>
 #include "classes/character.h"
+#include "classes/town.h"
 #include <conio.h>
 using namespace std;
 
-void displayMenu();
+void mainMenu();
+void startNewGame();
+void intro();
+int gameLoop();
+
+Town towns[6] = {Town("Whispering Pines", 6),
+                 Town("Everbrook", 4),
+                 Town("Moonhaven", 4),
+                 Town("Foggy Bottom", 7),
+                 Town("Stardust Valley", 3),
+                 Town("Crystal Lake", 2)};
 
 int main()
 {
-    displayMenu();
+    mainMenu();
     return 0;
 };
 
-void displayMenu()
+void mainMenu()
 {
+    int choice;
     // to do: if save file exists : load
-    cout << "1.New Game \n2.Exit" << endl; 
+    cout << "1.New Game \n2.Exit" << endl;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        startNewGame();
+        break;
+    }
+    case 2:
+    {
+        system("pause");
+        break;
+    }
+    default:
+    {
+        cout << "Invalid choice" << endl;
+        mainMenu();
+        break;
+    }
+    }
+}
+
+void startNewGame()
+{
+    intro();
+    while (gameLoop() == 0)
+    {
+    }
+}
+
+void intro()
+{
+    // TODO: intro
+    cout << "---------------------------------------------" << endl;
+    cout << "Once upon a time kan fi zunji...ismu Akram..." << endl;
+}
+
+int gameLoop()
+{
+    cout << "---------------------------------------------" << endl;
+    cout << "1.Talk to NPCs" << endl;
+    cout << "2.Go to other town" << endl;
+    cout << "3.Shop" << endl;
+    cout << "4.Exit game" << endl;
+    int choice;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        // TODO: List NPCs in town
+        cout << "you speak" << endl;
+        break;
+    }
+    case 2:
+    {
+        // TODO: List traversable towns
+        cout << "you travel" << endl;
+        break;
+    }
+    case 3:
+    {
+        // TODO: Open shop
+        cout << "you shop" << endl;
+        break;
+    }
+    case 4:
+    {
+        return 1;
+        break;
+    }
+    default:
+    {
+        cout << "Invalid choice" << endl;
+        gameLoop();
+        break;
+    }
+    }
+    return 0;
 }
