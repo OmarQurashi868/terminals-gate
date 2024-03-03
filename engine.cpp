faff845ecb0f7ae7bfad5c79374bd41a954d201b
Akram <tubeakram7@gmail.com>
2024-03-03 17:23:56 +0200

    ...

--- engine.cpp
+++ engine.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "classes/character.h"
+#include <conio.h>
 using namespace std;
+
+void displayMenu();
 
 int main()
 {
-    
+    displayMenu();
     return 0;
 };
+
+void displayMenu()
+{
+    // to do: if save file exists : load
+    cout << "1.New Game \n2.Exit" << endl; 
+}
